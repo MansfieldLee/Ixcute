4c221cf437150e254b3b7417219442e32a9cb5f5
will-hit <52225566+will-hit@users.noreply.github.com>
2019-11-06 15:18:53 +0800

    跳转出错

--- IxcuteWeb/src/org/hit/controller/LoginController.java
+++ IxcuteWeb/src/org/hit/controller/LoginController.java
@@ -1,7 +1,5 @@
 package org.hit.controller;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,23 +10,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("Login")
 public class LoginController {
-
+	
 	@Autowired
 	private LoginService loginService;
-
+	
 	@Autowired
 	private User user;
-
-	@Autowired
-	private ModelAndView mv; 
 	
-	@ResponseBody
+    
 	@RequestMapping("LoginServlet")
 	public String loginUser(@RequestParam("psd") String pwd,@RequestParam("user") String name,
 			@RequestParam("checkcode") String result,HttpServletRequest request) {
@@ -37,22 +30,25 @@ public class LoginController {
 		user.setUpwd(pwd);
 		User loginUser = loginService.selectloginbyname(user);
 		String Correct = (String) request.getSession().getAttribute("CHECKCODE");
+<<<<<<< HEAD
 		System.out.println(name + "-" + pwd + "-" + result + "-" + Correct);
 
 		if (loginUser != null && result.equals(Correct)) {
 			return "success";
 		} else {
 			return "forward:/index.jsp";
+=======
+		System.out.println(name+"-"+pwd+"-"+result+"-"+Correct);
+		
+		if(loginUser != null&&result.equals(Correct)) {
+			return "success";
+		}else {
+			return "redirect:/index.jsp";
+>>>>>>> parent of 875b0c2... Merge branch 'master' of https://github.com/will-hit/Ixcute
 		}
 	}
-
+	
 	public void setLoginService(LoginService loginService) {
 		this.loginService = loginService;
-	}
-
-
-
-	public void setModelAndView(ModelAndView modelAndView) {
-		this.mv = modelAndView;
 	}
 }
