9ca5376e0aa6bf243eb94a4d96c428a50d54a998
will-hit <52225566+will-hit@users.noreply.github.com>
2019-11-10 16:02:47 +0800

    安全性
    将view中的jsp页面加入WEEB-INF中，非请求无法访问

---
+++ IxcuteWeb/src/org/hit/authorizedInterceptor/AuthorizedInterceptor.java
@@ -0,0 +1,49 @@
+package org.hit.authorizedInterceptor;
+
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthorizedInterceptor implements HandlerInterceptor{
+	
+	//@Autowired
+	//private UserService userService;
+	
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+    	Cookie[] cookies = request.getCookies();
+		String uri = request.getRequestURI();
+		System.out.println("uri:"+uri);
+		if(uri.equals("/IxcuteWeb/Login/LoginServlet"))
+		{
+			return true;
+		}
+		
+		
+		
+		
+		
+		
+		response.sendRedirect("../index.jsp");
+		return true;
+		
+    }
+
+	
+	public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception{
+		System.out.println("postHandle");
+	}
+	
+	 public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+	        System.out.println("进入了afterCompletion方法！！！！");
+	    }
+
+	
+}
--- IxcuteWeb/src/org/hit/controller/LoginController.java
+++ IxcuteWeb/src/org/hit/controller/LoginController.java
@@ -1,18 +1,19 @@
 package org.hit.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.hit.entity.User;
 import org.hit.service.Impl.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("Login")
+@RequestMapping(value="Login")
 public class LoginController {
 	
 	@Autowired
@@ -24,18 +25,33 @@ public class LoginController {
     
 	@RequestMapping("LoginServlet")
 	public String loginUser(@RequestParam("psd") String pwd,@RequestParam("user") String name,
-			@RequestParam("checkcode") String result,HttpServletRequest request) {
-
+			@RequestParam("checkcode") String result,HttpServletRequest request,HttpServletResponse response) {
+		
 		user.setUname(name);
 		user.setUpwd(pwd);
 		User loginUser = loginService.selectloginbyname(user);
+		HttpSession session = request.getSession();
 		String Correct = (String) request.getSession().getAttribute("CHECKCODE");
 		System.out.println(name + "-" + pwd + "-" + result + "-" + Correct);
-
+		
 		if (loginUser != null && result.equals(Correct)) {
+			//success login!
+			session.setAttribute("username", name);
+			session.setAttribute("password",pwd);
+	        Cookie usernameCookie = new Cookie("username",name);
+	        usernameCookie.setMaxAge(500);
+	        usernameCookie.setPath("/");
+	        response.addCookie(usernameCookie);
 			return "success";
 		} else {
-			return "forward:/index.jsp";
+			return "redirect:/index.jsp";
+		}
+	}
+	
+	@RequestMapping(value="Next")
+	public String Next() {
+		System.out.println("COME HERE!");
+		return "next";
 	}
 	
 	public void setLoginService(LoginService loginService) {
